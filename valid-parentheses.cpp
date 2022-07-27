89c127196fe741fd4a1b7dd3b46e127b28f9d54e
TongKW <66624654+TongKW@users.noreply.github.com>
2022-07-27 23:36:05 +0800

    Create valid-parentheses.cpp

---
+++ valid-parentheses.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        // (  )  [  ]  {   }
+        // 40 41 91 93 123 125
+        stack<int> stack;
+        for(char& c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) return false;
+                char top = stack.top();
+                int diff = (int)c - (int)top;
+                if (diff != 1 && diff != 2) return false;
+                stack.pop();
+            }
+        }
+        return stack.empty();
+    }    
+};
